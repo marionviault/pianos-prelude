80d9479a456331864db304dc2809764c9999e81e
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-08-10 18:58:40 +0200

    :tada: init project

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Header/>
+    <Nuxt/>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <Navigation/>
+    <Footer/>
+  </div>
+</template>
