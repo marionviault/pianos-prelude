4a5c624e8fd73671170deffe0ae8c94cb9c8f7a8
Marion Viault <marion-viault@wanadoo.fr>
2023-09-11 00:39:13 +0200

    Replace footer img tags with next/image

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from "react";
+import Image from "next/image";
 
 const Footer: React.FC = () => {
 
@@ -8,17 +9,17 @@ const Footer: React.FC = () => {
             <div className="bg-blue-300 flex">
                 <Link href="https://www.facebook.com/">
                     <a target="_blank">
-                        <img src="/facebook-logo.png" alt="Facebook logo" width="60"/>
+                        <Image src="/facebook-logo.png" alt="Facebook logo" width="60" height="60"/>
                     </a>
                 </Link>
                 <Link href="https://twitter.com/?lang=fr">
                     <a target="_blank">
-                        <img src="/twitter-logo.png" alt="Twitter logo" width="60"/>
+                        <Image src="/twitter-logo.png" alt="Twitter logo" width="60" height="60"/>
                     </a>
                 </Link>
                 <Link href="https://www.instagram.com/">
                     <a target="_blank">
-                        <img src="/instagram-logo.png" alt="Instagram logo" width="60"/>
+                        <Image src="/instagram-logo.png" alt="Instagram logo" width="60" height="60"/>
                     </a>
                 </Link>
             </div>
