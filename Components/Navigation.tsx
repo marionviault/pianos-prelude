2e1c9a2d35810def39099fcb7e0a2d460d980aa3
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-11 23:12:24 +0200

    :bug: fix Navigation && use Next/Image

--- Components/Navigation.tsx
+++ Components/Navigation.tsx
@@ -1,129 +1,88 @@
 import Link from "next/link";
-import { NextRouter, useRouter } from "next/router";
+import {NextRouter, useRouter} from "next/router";
 import styles from "../styles/Navigation.module.scss";
 import React from "react";
-import { Disclosure } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import {Disclosure} from '@headlessui/react'
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
 import Image from "next/image";
-import { navItem } from "../interfaces/navigation";
+import {navItem} from "../interfaces/navigation";
 
 const NAVIGATION: navItem[] = [
-    { name: 'À propos', href: '/#a-propos', current: false },
-    { name: 'Prestations', href: '/#prestations', current: false },
-    { name: 'Pianos en vente', href: '/pianos', current: false },
+    {name: 'À propos', href: '/#a-propos', current: false},
+    {name: 'Prestations', href: '/#prestations', current: false},
+    {name: 'Pianos en vente', href: '/pianos', current: false},
 ]
 
-function classNames(...classes) {
+function classNames(...classes: string[]): string {
     return classes.filter(Boolean).join(' ')
 }
 
-// const Navigation: React.FC = () => {
-
-// const ROUTER: NextRouter = useRouter();
-//
-//     return (
-//         <div className="basis-2/12 text-white pl-4">
-//             <nav className="basis-2/12 text-white pl-4">
-//                 <div className={styles.navigation}>
-//                     <ul className="grid gap-4">
-//
-//                         <li className={`${ROUTER.pathname === "/" ? styles.active : ""} p-2`}>
-//                             <Link href="/">
-//                                 <a>ACCUEIL</a>
-//                             </Link>
-//                         </li>
-//
-//                         <li className="">
-//                             <a href="#piano-carousel">Liste des pianos</a>
-//                         </li>
-//
-//                         <li className={`${ROUTER.pathname === "/about" ? styles.active : ""} p-2`}>
-//                             <Link href="/about">
-//                                 <a>A PROPOS</a>
-//                             </Link>
-//                         </li>
-//
-//                     </ul>
-//                 </div>
-//             </nav>
-//         </div>
-//     );
-// }
-
-
-export default function Navigation(): React.FC {
+export default function Navigation(): JSX.Element {
 
     const ROUTER: NextRouter = useRouter();
 
     return (
         <Disclosure as="nav" className="bg-gray-800">
-            {({ open }) => (
+            {({open}) => (
                 <>
                     <div className={`${styles.navigation} mx-auto max-w-7xl px-2 sm:px-6 lg:px-8`}>
                         <div className="relative flex h-16 items-center justify-between">
+
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
                                 <Disclosure.Button
                                     className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="absolute -inset-0.5"/>
                                     <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
-                                    ) : (
-                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
-                                    )}
+                                    {open ? (<XMarkIcon className="block h-6 w-6" aria-hidden="true"/>) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>)}
                                 </Disclosure.Button>
                             </div>
+
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+
                                 <div className="flex flex-shrink-0 items-center">
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                                        alt="Your Company"
-                                    />
+                                    <Image className="h-8 w-auto" src="/twitter-logo.png" alt="Your Company" width="50"
+                                           height="50"/>
                                 </div>
+
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
-                                        {NAVIGATION.map((item: navItem) => (
-                                            <div key={item.name}
-                                                 className={ROUTER.pathname === item.href ? styles.active : ""}>
-                                                <Link href={item.href}>
-                                                    <a
-                                                        className={classNames(
-                                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                            'rounded-md px-3 py-2 text-sm font-medium'
-                                                        )}
-                                                        aria-current={item.current ? 'page' : undefined}
-                                                    >
-                                                        {item.name}
+                                        {NAVIGATION.map((navItem: navItem) => (
+                                            <div key={navItem.name}
+                                                 className={ROUTER.pathname === navItem.href ? styles.active : ""}>
+                                                <Link href={navItem.href}>
+                                                    <a className={classNames(
+                                                        navItem.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                        'rounded-md px-3 py-2 text-sm font-medium'
+                                                    )} aria-current={navItem.current ? 'page' : undefined}>
+                                                        {navItem.name}
                                                     </a>
                                                 </Link>
                                             </div>
                                         ))}
                                     </div>
                                 </div>
+
                             </div>
+
                         </div>
                     </div>
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
-                            {NAVIGATION.map((item: navItem) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block rounded-md px-3 py-2 text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                >
-                                    {item.name}
+                            {NAVIGATION.map((navItem: navItem) => (
+                                <Disclosure.Button key={navItem.name} as="a" href={navItem.href}
+                                                   className={classNames(
+                                                       navItem.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                       'block rounded-md px-3 py-2 text-base font-medium'
+                                                   )}
+                                                   aria-current={navItem.current ? 'page' : undefined}>
+                                    {navItem.name}
                                 </Disclosure.Button>
                             ))}
                         </div>
                     </Disclosure.Panel>
+
                 </>
             )}
         </Disclosure>
