9e868414fae034b998ac62744fe46310ddc29923
Marion Viault <marion-viault@wanadoo.fr>
2023-09-11 22:36:14 +0200

    Replace about page with pianos page

---
+++ pages/pianos/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PianoCarousel from "../../Components/PianoCarousel";
+
+const Pianos: React.FC = () => {
+
+    return (
+        <>
+
+            <h1>Page listant les pianos</h1>
+
+            <PianoCarousel/>
+
+        </>
+    )
+};
+
+export default Pianos;
