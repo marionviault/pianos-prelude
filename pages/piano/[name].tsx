da201d386864723b97c881ea1a1854789a09f178
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-21 23:37:06 +0200

    :rotating_light: log error

--- pages/piano/[name].tsx
+++ pages/piano/[name].tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import {useRouter} from "next/router";
 import useSWR from "swr";
+
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 
 const PianoFull = () => {
     const router = useRouter();
 
-    const { data, error } = useSWR('/api/staticdata', fetcher);
+    const {data, error} = useSWR('/api/staticdata', fetcher);
 
     //Handle the error state
-    if (error) return <div>Failed to load</div>;
+    if (error) {
+        console.log(error);
+        return <div>Failed to load</div>;
+    }
 
     let pianos = [];
     //Handle the loading state
