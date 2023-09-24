2bb465043e8d2d350e152efe5ad4eba70a6b9323
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-25 00:07:43 +0200

    update ContactForm (manage with mailto)

--- Components/sections/ContactForm.tsx
+++ Components/sections/ContactForm.tsx
@@ -1,14 +1,27 @@
 import Button from "./../Button";
-import React from "react";
+import React, {useState} from "react";
 
 const ContactForm: React.FC = () => {
+
+    const [subject, setSubject] = useState('antoine');
+
+    function handleSubject(event: { target: { value: React.SetStateAction<string>; }; }) {
+        setSubject(event.target.value)
+    }
 
     return (
         <form id="contact" className="flex">
 
             <div className="inline-flex basis-3/4 border-2 border-slate-400 rounded-lg">
 
-                <input className="w-full p-2  rounded-lg" type="text" placeholder="formulaire de contact"/>
+                <input className="w-full p-2 rounded-lg" type="email" placeholder="example@gmail.com"/>
+
+                <input className="w-full p-2 rounded-lg" type="text" placeholder="Devis, Contact, Vente, ..."
+                       onChange={handleSubject}/>
+
+                <input className="w-full p-2 rounded-lg" type="text" placeholder="Mon message"/>
+
+                <a href={`"mailto:vitkov@live.fr?subject=${subject}&body=Bla bla bla"`}>Mail to</a>
 
                 <Button label="Envoyer"
                         classList="bg-slate-400 border-2 border-slate-400 rounded-lg float-right text-white"/>
