064643a5fcbfef447fe0b0d139b447c02adae26a
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-25 17:44:47 +0200

    fix Next/Image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const withExportImages = require('next-export-optimize-images');
+
+module.exports = withExportImages({
   reactStrictMode: true,
-  images:{
-    domains:['images.unsplash.com']
+  images: {
+    domains: ['images.unsplash.com']
   }
-}
+})
