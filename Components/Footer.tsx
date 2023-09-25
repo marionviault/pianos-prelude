503c7094bfa031fa17647a7d26cba90667b2f663
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-25 16:19:45 +0200

    :arrow_up: upgrade dependencies && adapt <Link>

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -7,17 +7,17 @@ const Footer: React.FC = () => {
     return (
         <footer>
             <div className="bg-blue-300 flex">
-                <Link href="https://www.facebook.com/">
+                <Link legacyBehavior href="https://www.facebook.com/">
                     <a target="_blank">
                         <img src="/facebook-logo.png" alt="Facebook logo" width="60" height="60"/>
                     </a>
                 </Link>
-                <Link href="https://twitter.com/?lang=fr">
+                <Link legacyBehavior href="https://twitter.com/?lang=fr">
                     <a target="_blank">
                         <img src="/twitter-logo.png" alt="Twitter logo" width="60" height="60"/>
                     </a>
                 </Link>
-                <Link href="https://www.instagram.com/">
+                <Link legacyBehavior href="https://www.instagram.com/">
                     <a target="_blank">
                         <img src="/instagram-logo.png" alt="Instagram logo" width="60" height="60"/>
                     </a>
