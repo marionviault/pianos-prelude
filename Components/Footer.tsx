3686d50814790119411c91471533f4459e592636
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-25 17:45:00 +0200

    replace img by Next/Image

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -9,17 +9,17 @@ const Footer: React.FC = () => {
             <div className="bg-blue-300 flex">
                 <Link legacyBehavior href="https://www.facebook.com/">
                     <a target="_blank">
-                        <img src="/facebook-logo.png" alt="Facebook logo" width="60" height="60"/>
+                        <Image src="/facebook-logo.png" alt="Facebook logo" width="60" height="60"/>
                     </a>
                 </Link>
                 <Link legacyBehavior href="https://twitter.com/?lang=fr">
                     <a target="_blank">
-                        <img src="/twitter-logo.png" alt="Twitter logo" width="60" height="60"/>
+                        <Image src="/twitter-logo.png" alt="Twitter logo" width="60" height="60"/>
                     </a>
                 </Link>
                 <Link legacyBehavior href="https://www.instagram.com/">
                     <a target="_blank">
-                        <img src="/instagram-logo.png" alt="Instagram logo" width="60" height="60"/>
+                        <Image src="/instagram-logo.png" alt="Instagram logo" width="60" height="60"/>
                     </a>
                 </Link>
             </div>
