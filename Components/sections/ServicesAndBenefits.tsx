c90a1d51079ec16cdf64ecaf06e7f7db2b2d063f
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-27 00:16:48 +0200

    add Interface

--- Components/sections/ServicesAndBenefits.tsx
+++ Components/sections/ServicesAndBenefits.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import {
+    BellAlertIcon,
+    CogIcon,
+    MusicalNoteIcon,
+    ShoppingCartIcon,
+    WrenchScrewdriverIcon
+} from '@heroicons/react/24/outline'
 
-import {  BellAlertIcon, CogIcon, MusicalNoteIcon, ShoppingCartIcon, WrenchScrewdriverIcon } from '@heroicons/react/24/outline'
+interface ServiceProps {
+    name: string,
+    description: string,
+    icon: any
+}
 
-const services = [
+const services: Array<ServiceProps> = [
     {
         name: 'Accord',
         description:
@@ -56,11 +67,11 @@ const ServicesAndBenefits: React.FC = () => {
                     </div>
                     <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
                         <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-                            {services.map((service) => (
+                            {services.map((service: ServiceProps) => (
                                 <div key={service.name} className="relative pl-16">
                                     <h3 className="text-base font-semibold leading-7 text-gray-900">
                                         <div
-                                            className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                                            className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-red-800">
                                             <service.icon className="h-6 w-6 text-white" aria-hidden="true"/>
                                         </div>
                                         {service.name}
