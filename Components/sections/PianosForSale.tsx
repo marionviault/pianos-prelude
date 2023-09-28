29e24202a0eb10a2521f3d33a1eb7c8d6081cfd2
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-28 17:42:03 +0200

    :art: apply BGcolor on pianossale

--- Components/sections/PianosForSale.tsx
+++ Components/sections/PianosForSale.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Piano from "./../Piano";
 import PianosJSON from "./../../pianos.json"
 import {PianoProps} from "../../interfaces/piano";
+import styles from "../../styles/PianosForSale.module.scss";
 
 const PianosForSale: React.FC = () => {
 
     const PIANOS: Array<PianoProps> = PianosJSON.pianos;
 
     return (
-        <section id="pianos-en-vente" className="bg-red-300">
+        <section id="pianos-en-vente" className={`${styles.pianos}`}>
 
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
                 <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
