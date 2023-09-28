4d8195e0639e822f027b01a8f943496e4e71712e
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-09-28 02:25:26 +0200

    :art: clean module scss

--- Components/Navigation.tsx
+++ Components/Navigation.tsx
@@ -6,7 +6,6 @@ import {Disclosure} from '@headlessui/react'
 import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
 import Image from "next/image";
 import {navItem} from "../interfaces/navigation";
-import {main} from '../styles/colors.module.scss'
 
 const NAVIGATION: navItem[] = [
     {name: 'À propos', href: '/#a-propos', current: false},
@@ -17,7 +16,7 @@ const NAVIGATION: navItem[] = [
 ]
 
 function classNames(...classes: string[]): string {
-    return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(' ');
 }
 
 export default function Navigation(): JSX.Element {
@@ -25,10 +24,10 @@ export default function Navigation(): JSX.Element {
     const ROUTER: NextRouter = useRouter();
 
     return (
-        <Disclosure as="header" className="sticky top-0 z-10" style={{ backgroundColor: main}} >
+        <Disclosure as="header" className={`${styles.navigation} sticky top-0 z-10`}>
             {({open}) => (
                 <>
-                    <nav className={`${styles.navigation} mx-auto max-w-7xl px-2 sm:px-6 lg:px-8`}>
+                    <nav className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
 
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -47,7 +46,7 @@ export default function Navigation(): JSX.Element {
                                     <Image className="h-8 w-auto"
                                            src="/piano-icon.png" alt="Logo représentant des touches de piano"
                                            width="50" height="50"/>
-                                    <p className="pl-3 text-sm font-medium text-white">Pianos Prélude</p>
+                                    <p className="pl-3 text-sm font-medium">Pianos Prélude</p>
                                 </div>
 
                                 <div className="hidden sm:ml-6 sm:block">
@@ -56,10 +55,8 @@ export default function Navigation(): JSX.Element {
                                             <div key={navItem.name}
                                                  className={ROUTER.pathname === navItem.href ? styles.active : ""}>
                                                 <Link legacyBehavior href={navItem.href}>
-                                                    <a className={classNames(
-                                                        navItem.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                        'rounded-md px-3 py-2 text-sm font-medium'
-                                                    )} aria-current={navItem.current ? 'page' : undefined}>
+                                                    <a className='rounded-md px-3 py-2 text-sm font-medium'
+                                                       aria-current={navItem.current ? 'page' : undefined}>
                                                         {navItem.name}
                                                     </a>
                                                 </Link>
