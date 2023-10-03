a0953a470c72cec2f72317e23cf7025063e0c8b1
Marion Viault <marion-viault@wanadoo.fr>
2023-10-03 20:09:45 +0200

    Factoring Testimonies section

--- Components/sections/Testimonies.tsx
+++ Components/sections/Testimonies.tsx
@@ -9,9 +9,14 @@ interface TestimonyProps {
 
 const TESTIMONIES: Array<TestimonyProps> = [
     {
-        quote: 'quote',
-        author: 'author',
-        city: 'city'
+        quote: 'Je tiens à vous remercier de la grande qualité de votre savoir-faire. Vous avez procédé avec efficacité et professionnalisme à l’harmonisation, au réglage et à l’accord de mon piano.',
+        author: 'Mme MERLE-RAYMOND',
+        city: 'Orgerus'
+    },
+    {
+        quote: 'Nous sommes très reconnaissants pour tout le travail effectué sur notre vieux piano qui n’avait pas vu d’accordeur depuis plus de vingt ans… Nous sommes impressionnés par le professionnalisme de Robin, allié à un art de la pédagogie si bienveillante. Que dire aussi de son perfectionnisme qui ne ménage ni son temps ni sa peine. Bravo et merci encore.',
+        author: 'M. et Mme KLEINBERGER',
+        city: 'Longnes'
     }
 ]
 
@@ -25,56 +30,31 @@ const Testimonies: React.FC = () => {
                 </h2>
             </div>
 
-            <div className="mx-auto max-w-2xl lg:max-w-4xl">
-                <figure className="mt-10">
-                    <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-                        <p>
-                            “Je tiens à vous remercier de la grande qualité de votre savoir-faire. Vous avez procédé
-                            avec efficacité et professionnalisme à l’harmonisation, au réglage et à l’accord de mon piano.”
-                        </p>
-                    </blockquote>
-                    <figcaption className="mt-10">
-                        {/*<img*/}
-                        {/*    className="mx-auto h-10 w-10 rounded-full"*/}
-                        {/*    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"*/}
-                        {/*    alt=""*/}
-                        {/*/>*/}
-                        <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                            <div className="font-semibold text-gray-900">Mme MERLE-RAYMOND</div>
-                            <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                                <circle cx={1} cy={1} r={1} />
-                            </svg>
-                            <div className="text-gray-600">Orgerus</div>
-                        </div>
-                    </figcaption>
-                </figure>
-            </div>
-            <br/>
-            <div className="mx-auto max-w-2xl lg:max-w-4xl">
-                <figure className="mt-10">
-                    <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-                        <p>
-                            “Nous sommes très reconnaissants pour tout le travail effectué sur notre vieux piano qui n’avait pas vu d’accordeur depuis plus de vingt ans… Nous sommes impressionnés par le professionnalisme de Robin, allié à un art de la pédagogie si bienveillante.
-                            Que dire aussi de son perfectionnisme qui ne ménage ni son temps ni sa peine.
-                            Bravo et merci encore.”
-                        </p>
-                    </blockquote>
-                    <figcaption className="mt-10">
-                        {/*<img*/}
-                        {/*    className="mx-auto h-10 w-10 rounded-full"*/}
-                        {/*    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"*/}
-                        {/*    alt=""*/}
-                        {/*/>*/}
-                        <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                            <div className="font-semibold text-gray-900">M. et Mme KLEINBERGER</div>
-                            <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
-                                <circle cx={1} cy={1} r={1} />
-                            </svg>
-                            <div className="text-gray-600">Longnes</div>
-                        </div>
-                    </figcaption>
-                </figure>
-            </div>
+            {TESTIMONIES.map((testimony: TestimonyProps) => (
+                <div className="mx-auto max-w-2xl lg:max-w-4xl">
+                    <figure className="mt-10">
+                        <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+                            <p>
+                                “{testimony.quote}”
+                            </p>
+                        </blockquote>
+                        <figcaption className="mt-10">
+                            {/*<img*/}
+                            {/*    className="mx-auto h-10 w-10 rounded-full"*/}
+                            {/*    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"*/}
+                            {/*    alt=""*/}
+                            {/*/>*/}
+                            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                                <div className="font-semibold text-gray-900">{testimony.author}</div>
+                                <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                                    <circle cx={1} cy={1} r={1} />
+                                </svg>
+                                <div className="text-gray-600">{testimony.city}</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+            ))}
 
         </section>
     );
