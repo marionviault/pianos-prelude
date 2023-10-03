f53c49adde18a9e9669a029de4c14dd8dbad8517
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-10-03 22:03:39 +0200

    update Piano text section

--- Components/sections/PianosForSale.tsx
+++ Components/sections/PianosForSale.tsx
@@ -9,14 +9,21 @@ const PianosForSale: React.FC = () => {
     const PIANOS: Array<PianoProps> = PianosJSON.pianos;
 
     return (
-        <section className={`${styles.pianos} px-6 pt-20 sm:py-8 lg:px-8`}>
+        <section className={`${styles.pianos} px-6 pt-20 sm:py-8 lg:px-8 text-white`}>
             <span className="anchor" id="pianos-en-vente"></span>
 
             <div className="mx-auto max-w-2xl lg:max-w-7xl lg:grid-cols-2">
 
-                <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl pb-2">
                     Mes pianos disponibles à la vente
                 </h2>
+
+                <div>
+                    Bénéficiez d’une remise en état complète et d’une préparation exhaustive sur d’excellents pianos
+                    d’occasions choisis par mes soins et d’un rapport qualité/prix imbattable.
+                    Mes années en entreprise m’ont permis de savoir quels pianos sont les plus pertinents à vous
+                    proposer sur le plan mécanique, acoustique, et aussi économique pour combler vos projets.
+                </div>
 
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {PIANOS.map((piano: PianoProps, index: React.Key) => {
