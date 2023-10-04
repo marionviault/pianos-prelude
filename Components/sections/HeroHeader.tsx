f31912028a77a60fc189c15a4b44a39da52525a0
Witkowski Antoine <witkowski.antoine@gmail.com>
2023-10-04 10:31:35 +0200

    clean meta / banner

--- Components/sections/HeroHeader.tsx
+++ Components/sections/HeroHeader.tsx
@@ -6,7 +6,7 @@ import {bitter} from "../../pages/_app";
 
 const HeroHeader: React.FC = () => {
     return (
-        <section className={`${styles.heroHeader} relative isolate overflow-hidden py-20 sm:py-32`}>
+        <section className={`${styles.heroHeader} relative isolate overflow-hidden py-20 sm:py-32 text-white`}>
             <span className="anchor" id="hero-banner"></span>
 
             <Image src="https://images.pexels.com/photos/6073196/pexels-photo-6073196.jpeg"
@@ -16,12 +16,12 @@ const HeroHeader: React.FC = () => {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-7xl lg:mx-0">
 
-                    <h1 className={`${bitter.className} text-4xl text-center font-bold tracking-tight text-white sm:text-6xl`}>
-                        PIANOS PRELUDE<br/>au service de la musique
+                    <h1 className={`${bitter.className} text-4xl text-center font-bold tracking-tight sm:text-6xl`}>
+                        PIANOS PRELUDE<br/>au service de votre musique
                     </h1>
 
-                    <p className="mt-6 text-center text-lg leading-8 text-gray-300">
-                        Robin Lefèvre, technicien accordeur de pianos en région parisienne.
+                    <p className="mt-6 text-center text-lg leading-8">
+                        Robin Lefèvre, technicien accordeur en région parisienne.
                     </p>
 
                 </div>
