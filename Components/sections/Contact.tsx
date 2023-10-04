97b649e3c2fa09a241b14e0cf7a8dd24970a97e1
Marion Viault <marion-viault@wanadoo.fr>
2023-10-04 13:51:56 +0200

    :building_construction: Update contact form

--- Components/sections/Contact.tsx
+++ Components/sections/Contact.tsx
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Contact.module.scss";
 
 const Contact: React.FC = () => {
 
-    const [name, setName] = useState('');
-    const [body, setBody] = useState('');
+    const [ name, setName ] = useState('');
+    const [ phone, setPhone ] = useState('');
+    const [ message, setMessage ] = useState('');
 
     function handleName(event: { target: { value: React.SetStateAction<string>; }; }): void {
         setName(event.target.value);
     }
 
-    function handleBody(event: { target: { value: React.SetStateAction<string>; }; }): void {
-        setBody(event.target.value);
+    function handlePhone(event: { target: { value: React.SetStateAction<string>; }; }): void {
+        setPhone(event.target.value);
     }
+
+    function handleMessage(event: { target: { value: React.SetStateAction<string>; }; }): void {
+        setMessage(event.target.value);
+    }
+
+    const MAIL_CONTENT: string =
+        `Nom : ${name}
+        Tel : ${phone}
+        
+        Message :
+        
+        ${message}`;
 
     return (
         <section className={`${styles.contact} isolate px-6 pt-20 sm:py-8 lg:px-8`}>
@@ -22,21 +35,22 @@ const Contact: React.FC = () => {
                 <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
                     Contact
                 </h2>
-                <p className="mt-2 text-lg leading-8">
+                <p className="mt-2 text-base leading-7">
                     Pour toute question, demande de devis, projet particulier
                 </p>
             </div>
 
-            <form className="mx-auto mt-16 max-w-xl sm:mt-10">
+            <form className="mx-auto mt-16 max-w-xl sm:mt-10"
+                  action={`mailto:vitkov@live.fr?subject=[PIANOS-PRELUDE] Contact&body=${encodeURIComponent(MAIL_CONTENT)}`}>
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
 
                     <div>
                         <label htmlFor="first-name" className="block text-sm font-semibold leading-6">
-                            Nom *
+                            Nom / Société
                         </label>
                         <div className="mt-2.5">
                             <input type="text" name="first-name" autoComplete="given-name" onChange={handleName}
-                                   className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset sm:text-sm sm:leading-6"/>
+                                   className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
 
@@ -44,40 +58,19 @@ const Contact: React.FC = () => {
                         <label htmlFor="phone-number" className="block text-sm font-semibold leading-6">
                             Téléphone
                         </label>
-                        <div className="relative mt-2.5">
-                            <input type="tel" name="phone-number" autoComplete="tel"
-                                   className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-                        </div>
-                    </div>
-
-                    <div>
-                        <label htmlFor="piano-infos" className="block text-sm font-semibold leading-6">
-                            Votre piano *
-                        </label>
                         <div className="mt-2.5">
-                            <input type="text" name="piano-infos"
-                                   className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-                        </div>
-                    </div>
-
-                    <div>
-                        <label htmlFor="last-maintenance"
-                               className="block text-sm font-semibold leading-6">
-                            Date du dernier entretien
-                        </label>
-                        <div className="mt-2.5">
-                            <input type="text" name="last-maintenance"
-                                   className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                            <input type="tel" name="first-name" autoComplete="tel" required onChange={handlePhone}
+                                   className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
 
                     <div className="sm:col-span-2">
                         <label htmlFor="message" className="block text-sm font-semibold leading-6">
-                            Votre demande *
+                            Votre demande
                         </label>
                         <div className="mt-2.5">
-                                <textarea name="message" rows={4} onChange={handleBody}
-                                          className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                <textarea name="message" rows={4} onChange={handleMessage}
+                                          className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
 
@@ -90,10 +83,10 @@ const Contact: React.FC = () => {
 
                 </div>
                 <div className="mt-10">
-                    <a href={`mailto:vitkov@live.fr?subject=[PIANOS-PRELUDE] Contact&body=${encodeURIComponent(body)}`}
-                       className="block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    <button type="submit"
+                           className="block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm">
                         Me contacter
-                    </a>
+                    </button>
                 </div>
             </form>
 
